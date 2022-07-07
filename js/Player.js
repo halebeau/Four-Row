af0994fe71a083700068ddac890f828acf1716b7
Beau <halebeau3@gmail.com>
2022-07-07 14:36:32 -0500

    Added: favicon, and js files with classes

---
+++ js/Player.js
@@ -0,0 +1,10 @@
+// 1. How will you differentiate between player objects? Will players have a name, or an ID? How would an ID be created or set?
+// A- Create one player class.  Give each player(2), it's own ID.  Either set ID with 2 players or use the DOM to create ID.  
+
+// 2. How will a player's tokens be stored? How many tokens should each player have? How will you store or set that number?
+// A- Tokens should be stored in array, assigned to players ID. Players should have an equal amount of tokens, that are divided by the number of spaces on board.  
+
+// 3. How will each player's tokens be differentiated from the other player's, visually?
+// A- 
+
+class Player {}
